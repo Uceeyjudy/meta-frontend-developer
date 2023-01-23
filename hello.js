0e03a88234c72457c997c11bbaf2115daa2c8611
Uceeyjudy <ucjudith@gmail.com>
2023-01-23 15:19:11 +0100

    added javascript basic functions

---
+++ hello.js
@@ -0,0 +1,18 @@
+
+// for (var i = 100; i > 10; i = i - 10) {
+//     for (var j = 10; j > 4; j = j - 5) {
+//         console.log(i + " divided by " + j + " equals " + i / j);
+//     }
+// }
+// var arrOfKeys = ['speed', 'altitude', 'color'];
+// var drone = {
+//     speed: 100,
+//     altitude: 200,
+//     color: "red"
+// }
+// for (var i = 0; i < arrOfKeys.length; i++) {
+//     console.log(drone[arrOfKeys[i]])
+// }
+// console.log(drone)
+var letter 
+console.log(letter)
